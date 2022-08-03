12827bb11bf33ce6146c6a8eda93085fc4a9a4a5
lijiaxing <ljx569932357@gmail.com>
2022-08-03 23:30:44 +0800

    feat: 添加 apt demo

--- app/src/main/java/com/egas/demo/MainActivity.kt
+++ app/src/main/java/com/egas/demo/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.egas.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.egas.demo.base.BaseActivity
+import com.gas.annotation.BindView
+import com.gas.library.BindViewTools
 
 class MainActivity : BaseActivity() {
+
+    @BindView(R.id.textView)
+    lateinit var mTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        BindViewTools.bind(this)
+        mTextView.text = "aptdemo"
     }
+
 }
---
+++ apt-library/src/main/java/com/gas/library/BindViewTools.java
@@ -0,0 +1,19 @@
+package com.gas.library;
+
+import android.app.Activity;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class BindViewTools {
+   public static void bind(Activity activity) {
+      Class<? extends Activity> clazz = activity.getClass();
+      try {
+         Class<?> bindViewClass = Class.forName(clazz.getName() + "_ViewBinding");
+         Method method = bindViewClass.getMethod("bind", activity.getClass());
+         method.invoke(bindViewClass.newInstance(), activity);
+      } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
+         e.printStackTrace();
+      }
+   }
+}
