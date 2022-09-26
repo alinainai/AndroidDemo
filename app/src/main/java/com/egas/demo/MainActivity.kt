0707cc2734cb0a15aa37b10ef676620dbc340b63
alinainai <569932357@qq.com>
2022-09-26 16:37:11 +0800

    Update MainActivity.kt

--- app/src/main/java/com/egas/demo/MainActivity.kt
+++ app/src/main/java/com/egas/demo/MainActivity.kt
@@ -3,10 +3,26 @@ package com.egas.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.egas.demo.base.BaseActivity
+import com.egas.demo.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    private external fun stringFromJNI(): String
+
+    companion object {
+        init {
+            System.loadLibrary("demo")
+        }
     }
 }
