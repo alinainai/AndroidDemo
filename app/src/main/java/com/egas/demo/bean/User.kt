a257f7f6115e3b2b96a683726bd3751c8124e35c
alinainai <569932357@qq.com>
2022-09-26 14:39:58 +0800

    Update User.kt

--- app/src/main/java/com/egas/demo/bean/User.kt
+++ app/src/main/java/com/egas/demo/bean/User.kt
@@ -1,13 +1,12 @@
 package com.egas.demo.bean
 
+import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class User(var name: String? = "") : Parcelable {
-//    private fun readFromParcel(source: Parcel) {
-//        this.uId = source.readInt()
-//        this.name = source.readString() ?: ""
-//        this.des = source.readString() ?: ""
-//    }
+data class User(var name: String?="") : Parcelable {
+    fun readFromParcel(source: Parcel) {
+        this.name = source.readString() ?: ""
+    }
 }
