6e1642c160b42355fe30a86463deda84efb324a0
alinainai <569932357@qq.com>
2022-09-26 16:40:52 +0800

    Update MainActivity.kt

--- app/src/main/java/com/egas/demo/MainActivity.kt
+++ app/src/main/java/com/egas/demo/MainActivity.kt
@@ -5,8 +5,14 @@ import android.os.Bundle
 import com.egas.demo.base.BaseActivity
 
 class MainActivity : BaseActivity() {
+   private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sampleText.text = "main page"
     }
 }
