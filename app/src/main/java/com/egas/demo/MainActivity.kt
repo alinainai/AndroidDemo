8177fc67e795b93a6331c4c0b606dbaa11877fcb
lijiaxing <ljx569932357@gmail.com>
2022-11-21 09:16:09 +0800

    feat: 添加打印 ClassLoader 的方法

--- app/src/main/java/com/egas/demo/MainActivity.kt
+++ app/src/main/java/com/egas/demo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.egas.demo
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
 import com.egas.demo.base.BaseActivity
 import com.egas.demo.databinding.ActivityMainBinding
 
@@ -12,7 +14,16 @@ class MainActivity : BaseActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.sampleText.text = "main page"
+    }
+
+    /**
+     * 打印ClassLoader
+     */
+    private fun logClassLoader(){
+        println("MainActivity.class=${this.classLoader}")
+        println("Context.class=${Context::class.java.classLoader}")
+        println("View.class=${View::class.java.classLoader}")
+        println("SystemClassLoader.class=${ClassLoader.getSystemClassLoader()}")
     }
 }
